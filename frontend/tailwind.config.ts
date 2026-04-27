db0fee238228ae6bdc9ed3c840f5f4c01fa92006
warren <warren.chen830@gmail.com>
2026-04-27 14:05:49 +0800

    refactor(frontend): remove darkMode from tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
 
 export default {
-  darkMode: 'class',
   content: [
     './components/**/*.{js,vue,ts}',
     './layouts/**/*.vue',
